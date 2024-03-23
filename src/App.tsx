e7cefc4e33fd54ada2bd810d64b216f984d323e9
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-03-23 23:33:49 +0900

    Project Start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+    ],
+  },
+});
