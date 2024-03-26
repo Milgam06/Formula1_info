094473ac93d05cc6989299a66693253b3191837f
Milgam06 <milgamfruit@gmail.com>
2024-03-26 22:29:34 +0900

    feat(page): add react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export const App: React.FC = () => {
+  const client = new QueryClient();
   return (
     <div>
       <h1>App</h1>
