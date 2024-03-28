4d7eeb6a24090387549f4790b0b108e3a54165e4
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-03-29 02:27:27 +0900

    feat(api): add api suffix

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export interface F1ResponseType {
+  driver_number: number;
+  session_key: string;
+}
+
+export const APP_SUFFIX = {
+  BASEURL: "https://api.openf1.org/v1", // base url
+  INTERVAL: "/intervals?session_key=latest&driver_number=", // interval times
+  LAPS: "/laps?session_key=latest&driver_number=", // lap times
+  POSITION: "/positions?session_key=latest&driver_number=", // driver position
+  SESSION: "/sessions?session_key=latest", // session info
+  MEETING: "/meetings?", // meeting info
+  STINTS: "/stints?session_key=latest&driver_number=", // pit stops info
+};
+
+export const instance = axios.create({
+  baseURL: APP_SUFFIX.BASEURL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
