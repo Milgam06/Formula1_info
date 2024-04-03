fbde6ae402c0f2a520eb8262cd1e71d94070e053
Milgam06 <milgamfruit@gmail.com>
2024-04-04 00:29:52 +0900

    feat(page): add async function to get respone

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
+
 import { FormulaDatas } from "@/api";
 
 export const MainPage: React.FC = () => {
   const formula = new FormulaDatas();
+  const [sessionStatus, setSessionStatus] = useState<string[]>([]);
 
-  const respone = formula.getSessionStatus();
-  console.log(respone);
+  useEffect(() => {
+    const fetchData = async () => {
+      const respone = await formula.getSessionStatus();
+      setSessionStatus(respone);
+    };
+    fetchData();
+  }, [sessionStatus]);
+  console.log(sessionStatus);
   return (
     <>
       <h1>asdf</h1>
