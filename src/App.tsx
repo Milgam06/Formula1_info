94c85cc40feb7f9047f015140979846c2bc9f579
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-04-05 03:42:37 +0900

    feat(style): add globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Global } from "@emotion/react";
+import { globalStyle } from "@/styles/global";
 import { Router } from "./router";
 
 export const App: React.FC = () => {
   return (
     <>
+      <Global styles={globalStyle} />
       <Router />
     </>
   );
