6eeaf3523c775fd98e1b7866aac3d54e6c226437
Milgam06 <milgamfruit@gmail.com>
2024-04-05 22:48:13 +0900

    feat(page): add main Page style

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,22 +1,47 @@
 import { useEffect, useState } from "react";
 
 import { FormulaDatas } from "@/api";
+import * as S from "./styled";
 
 export const MainPage: React.FC = () => {
   const formula = new FormulaDatas();
   const [sessionStatus, setSessionStatus] = useState<string[]>([]);
 
   useEffect(() => {
+    // const fetchData = async () => {
+    //   const respone = await formula.getSessionStatus({
+    //     sessionLatestStatus: true,
+    //   });
+    //   setSessionStatus(respone);
+    // };
+
+    // const fetchData = async () => {
+    // const respone = await formula.getIntervalStatus("1", {
+    //   sessionLatestStatus: true,
+    // });
+    // setSessionStatus(respone);
+    // };
+
+    // const fetchData = async () => {
+    //   const respone = await formula.getLapStatus("44");
+    //   setSessionStatus(respone);
+    // };
+
+    // const fetchData = async () => {
+    //   const respone = await formula.getPositionStatus("44");
+    //   setSessionStatus(respone);
+    // };
     const fetchData = async () => {
-      const respone = await formula.getSessionStatus();
+      const respone = await formula.getCarStatus("22");
       setSessionStatus(respone);
     };
+
     fetchData();
   }, []);
-  console.log(sessionStatus);
+  console.log(sessionStatus, "max");
   return (
     <>
-      <h1>asdf</h1>
+      <S.MainPageContainer>asdf</S.MainPageContainer>
     </>
   );
 };
---
+++ src/pages/main/styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const MainPageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+`;
