3d8b9bbf887ba73eccbb7784f575269c8107b314
Milgam06 <milgamfruit@gmail.com>
2024-04-05 22:36:08 +0900

    feat(component): set NavbarContentContainer

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Text } from "..";
 import * as S from "./styled";
 
 export const Navbar: React.FC = () => {
   return (
     <>
-      <S.NavbarContainer>asd</S.NavbarContainer>
+      <S.NavbarContainer>
+        <S.NavbarContentsContainer>
+          <S.NavbarTitleWrapper>
+            <Text size={3} weight={800} colors="#fff">
+              FORMULA
+            </Text>
+          </S.NavbarTitleWrapper>
+          <S.NavbarMenuContainer>
+            <span>Home</span>
+            <span>Drivers</span>
+            <span>Teams</span>
+          </S.NavbarMenuContainer>
+          afds
+        </S.NavbarContentsContainer>
+      </S.NavbarContainer>
     </>
   );
 };
--- src/components/Navbar/styled.ts
+++ src/components/Navbar/styled.ts
@@ -2,11 +2,40 @@ import { motion } from "framer-motion";
 import styled from "@emotion/styled";
 
 export const NavbarContainer = styled(motion.div)`
-  width: 10rem;
+  width: 20rem;
   height: 100%;
   left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #841613;
+`;
+
+export const NavbarContentsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+`;
+
+export const NavbarTitleWrapper = styled.div`
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #fff;
+  padding: 0 0.4rem;
+  margin: 0 0.4rem;
+`;
+
+export const NavbarMenuContainer = styled.div`
+  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #fff;
 `;
