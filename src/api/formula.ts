e28547eddc9803c10546caa29cecb7a6410a72b0
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-04-15 13:59:52 +0900

    feat(api): add ClassStates

--- src/api/formula.ts
+++ src/api/formula.ts
@@ -1,12 +1,29 @@
 import { API_SUFFIX, instance } from "./api";
 
 export interface FormulaPropsType {
-  driverNumber: string;
-  sessionLatestStatus: boolean;
-  sessionKey: string;
+  driverNumber?: string;
+  sessionLatestStatus?: boolean;
+  sessionKey?: string | "";
 }
 
 export class FormulaDatas {
+  FormulaState = {
+    intervalData: null,
+    sessionData: null,
+    lapData: null,
+    positionData: null,
+    carData: null,
+  };
+  constructor() {
+    this.FormulaState;
+  }
+  setState();
+
+  /**
+   * @param sessionLatestStatus: boolean
+   * @param sessionKey: string
+   * @param driverNumber: string
+   */
   async getIntervalStatus(props: FormulaPropsType) {
     try {
       const sessionKey = props.sessionLatestStatus
@@ -23,13 +40,16 @@ export class FormulaDatas {
     }
   }
 
+  /**
+   * @param sessionLatestStatus: boolean
+   * @param sessionKey: string
+   */
   async getSessionStatus(props: FormulaPropsType) {
     try {
       const sessionKey = props.sessionLatestStatus
         ? "latest"
         : props.sessionKey;
       const res = await instance.get(API_SUFFIX.SESSION + sessionKey);
-      console.log(res.data[0]);
       return res.data[0];
     } catch (error) {
       console.error(error);
@@ -37,10 +57,14 @@ export class FormulaDatas {
     }
   }
 
+  /**
+   * @param sessionLatestStatus: boolean
+   * @param sessionKey: string
+   * @param driverNumber: string
+   */
   async getLapStatus(props: FormulaPropsType) {
     try {
       const res = await instance.get(API_SUFFIX.LAPS + props.driverNumber);
-      console.log(res.data[0]);
       return res.data[0];
     } catch (error) {
       console.error(error);
@@ -48,20 +72,29 @@ export class FormulaDatas {
     }
   }
 
+  /**
+   * @param sessionLatestStatus: boolean
+   * @param sessionKey: string
+   * @param driverNumber: string
+   */
   async getPositionStatus(props: FormulaPropsType) {
     try {
       const res = await instance.get(API_SUFFIX.POSITION + props.driverNumber);
-      console.log(res.data[0]);
       return res.data[0];
     } catch (error) {
       console.error(error);
       return 0;
     }
   }
+
+  /**
+   * @param sessionLatestStatus: boolean
+   * @param sessionKey: string
+   * @param driverNumber: string
+   */
   async getCarStatus(props: FormulaPropsType) {
     try {
       const res = await instance.get(API_SUFFIX.CARS + props.driverNumber);
-      console.log(res.data[0]);
       return res.data[0];
     } catch (error) {
       console.error(error);
