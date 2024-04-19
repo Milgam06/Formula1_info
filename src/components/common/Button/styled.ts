93b4de29e8170788539e98b644831845c06351db
Milgam06 <milgamfruit@gmail.com>
2024-04-19 22:45:52 +0900

    fix(component): fix Button Container's padding

--- src/components/common/Button/styled.ts
+++ src/components/common/Button/styled.ts
@@ -11,7 +11,7 @@ export const ButtonContainer = styled(motion.div)<ButtonStyleProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 2rem 2.2rem;
+  padding: 2rem 2.6rem;
   border: none;
   border-radius: 2rem;
   cursor: pointer;
@@ -21,8 +21,8 @@ export const ButtonContentContainer = styled.div<ButtonStyleProps>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: ${(props) => (props.isLarge ? "84rem" : "30rem")};
+  width: ${(props) => (props.isLarge ? "76rem" : "30rem")};
   height: ${(props) => (props.isLarge ? "24rem" : "14rem")};
-  border: 1px solid #000;
+  /* border: 1px solid #000; */
   /* gap: 1rem; */
 `;
