15e8df73b0e57f129cb3bdf315eddf9e7be45e0a
Milgam06 <milgamfruit@gmail.com>
2024-05-07 09:08:10 +0900

    feat(hook): use falseIntervalStatus and trueIntervalStatus in useIntervalLiveCheck hook

--- src/hooks/queries/checkLive.ts
+++ src/hooks/queries/checkLive.ts
@@ -1,13 +1,29 @@
 import { useState, useEffect } from "react";
 
-import { getIntervalData, FormulaPropsType } from "@/api";
-import { useIntervalStore, IntervalStore } from "@/store";
+import { getIntervalData } from "@/api";
+import { useIntervalStore } from "@/store/interval";
 
+/**
+ * useIntervalLiveCheck
+ * @summary check live session & set interval status
+ */
 export const useIntervalLiveCheck = () => {
   const [liveCheck, setLiveCheck] = useState<boolean>(false);
-  const { intervalStatus, switchIntervalStatus } = useIntervalStore();
+  const { intervalStatus, trueIntervalStatus, falseIntervalStatus } =
+    useIntervalStore();
 
   useEffect(() => {
-    setInterval();
+    setInterval(async () => {
+      const respone = await getIntervalData({ sessionLatestStatus: true });
+      console.log(respone, "15s");
+      if (respone === undefined) {
+        setLiveCheck(false);
+        falseIntervalStatus();
+      } else if (respone !== undefined) {
+        setLiveCheck(true);
+        trueIntervalStatus();
+      }
+      console.log(intervalStatus, liveCheck, "intervalStatus");
+    }, 15000);
   }, []);
 };
