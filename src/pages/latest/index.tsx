8540ed06d365b32e9c29b356014f7476f502a15b
Milgam06 <milgamfruit@gmail.com>
2024-06-05 16:32:13 +0900

    feat(page): add LatestPage

---
+++ src/pages/latest/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styled";
+
+export const LatestPage: React.FC = () => {
+  return (
+    <>
+      <S.LatestPageContainer>
+        <h1>hello</h1>
+      </S.LatestPageContainer>
+    </>
+  );
+};
